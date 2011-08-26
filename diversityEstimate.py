97442947d84be82aaf30c164daa76f57dafb097c
Tim Vaughan <tgvaughan@gmail.com>
2011-08-26 16:13:56 +1000

    Revamped diversity estimation.  Again.

--- diversityEstimate.py
+++ diversityEstimate.py
@@ -3,7 +3,7 @@
 from scipy import *
 
 # Number of sequences distance hp from 0 and 1 from a sequence at h:
-def gcond(hp,h,L):
+def g(hp,h,L):
 
 	if hp == h-1:
 		return h
@@ -18,55 +18,61 @@ def gcond(hp,h,L):
 	
 
 # Derivatives:
-def ddt (Y,V, L, p):
+def ddt (X, Y, V, L, p):
 
 	# Rate of mutation to neighbouring sequences:
-	mup = p['mu']/(3*L)
+	mup = p['mu']/(3.*L)
 
-	dYdt = (1.-p['mu'])*p['beta']*p['xbar']*V
+	dXdt = p['lambda'] - p['d']*X
+	for h in range(L+1):
+		dXdt -= p['beta']*X*V[h]
+	
+	dYdt = (1.-p['mu'])*p['beta']*X*V - p['a']*Y
+	for h in range(L+1):
 
-	for h in range(len(Y)):
-
-		dYdt[h] += mup*p['beta']*p['xbar']*gcond(h,h,L)*V[h]
+		dYdt[h] += mup*p['beta']*X*g(h,h,L)*V[h]
 
 		if h>0:
-			dYdt[h] += mup*p['beta']*p['xbar']*gcond(h-1,h,L)*V[h-1]
+			dYdt[h] += mup*p['beta']*X*g(h-1,h,L)*V[h-1]
 
-		if h<L:
-			dYdt[h] += mup*p['beta']*p['xbar']*gcond(h+1,h,L)*V[h+1]
+		if h<0:
+			dYdt[h] += mup*p['beta']*X*g(h+1,h,L)*V[h+1]
 	
-	dVdt = p['k']*Y - p['beta']*p['xbar']*V
+	dVdt = p['k']*Y - p['beta']*X*V - p['u']*V
 
-	return (dYdt,dVdt)
+	return (dXdt, dYdt,dVdt)
 
 # Semi-implicit integration step:
-def step(Y, V, dt, L, p):
+def step(X, Y, V, dt, L, p):
 
+	Xp = X
 	Yp = Y.copy()
 	Vp = V.copy()
 
 	for i in range(3):
-		dYdt,dVdt = ddt(Y, V, L, p)
+		dXdt,dYdt,dVdt = ddt(X, Y, V, L, p)
+		Xp = X + 0.5*dt*dXdt
 		Yp = Y + 0.5*dt*dYdt
 		Vp = V + 0.5*dt*dVdt
 	
+	X = 2.*Xp - X
 	Y = 2.*Yp - Y
 	V = 2.*Vp - V
 
-	return (Y, V)
+	return (X, Y, V)
 
 # Send sample to stdout:
-def sample(V, t):
-	print t,
-	for i in range(len(V)):
-		print V[i],
+def sample(X, Y, V, t):
+	print t, X,
+	for h in range(len(V)):
+		print Y[h], V[h],
 	print
 
 # Produce header for sample file:
-def header(V):
-	print 't',
-	for i in range(len(V)):
-		print 'n'+str(i),
+def header(L):
+	print 't X',
+	for h in range(L+1):
+		print 'Y'+str(h), 'V'+str(h),
 	print
 
 
@@ -76,13 +82,16 @@ if __name__ == '__main__':
 	# Model parameters:
 	p = {}
 	L = 105
-	p['beta'] = 5e-13
+	p['lambda'] = 2.5e8
 	p['k'] = 1e3
-	p['xbar'] = 2.5e11
+	p['beta'] = 5e-13
 	p['mu'] = 2e-5*L
+	p['d'] = 1e-3
+	p['a'] = 1.
+	p['u'] = 3.
 
 	# Simulation parameters:
-	T = 2.
+	T = 10.
 	Nt = 1001
 	Nsamples = 101
 
@@ -92,22 +101,23 @@ if __name__ == '__main__':
 
 	# Initial condition:
 
+	X = p['lambda']/p['d']  # Uninfected steady state
 	Y = zeros(L+1)
 	V = zeros(L+1)
 	V[0] = 1
 
 	# Generate header:
-	header(V)
+	header(L)
 
 	# Perform first sample:
-	sample(V, 0)
+	sample(X, Y, V, 0)
 
 	# Integration loop:
 	for tidx in range(Nt):
 
 		# Perform semi-implicit integration step:
-		Y,V = step(Y, V, dt, L, p)
+		X,Y,V = step(X, Y, V, dt, L, p)
 
 		# Sample if necessary:
 		if tidx % steps_per_sample == 0:
-			sample(V, tidx*dt)
+			sample(X, Y, V, tidx*dt)
