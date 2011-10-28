fb039fb0b9eb862574edd14afc33e887492fd89a
Tim Vaughan <tgvaughan@gmail.com>
2011-10-29 00:45:04 +1100

    Renamed diversityEstimate.py.
    This script is actually a general integrator of the deterministic
    evolutionary model, and the new name reflects this.

--- deterministic.py
+++ deterministic.py
@@ -80,33 +80,33 @@ def header(L):
 if __name__ == '__main__':
 
 	# Model parameters:
-	#p = {}
-	#L = 35
-	#p['lambda'] = 1e5
-	#p['k'] = 100.
-	#p['beta'] = 2e-7
-	#p['mu'] = 2e-5*L
-	#p['d'] = 0.1
-	#p['a'] = 0.5
-	#p['u'] = 5.0
-	#p['V0'] = 100
+	p = {}
+	L = 1
+	p['lambda'] = 1e5
+	p['k'] = 100.
+	p['beta'] = 2e-7
+	p['mu'] = 0.0
+	p['d'] = 0.1
+	p['a'] = 0.5
+	p['u'] = 5.0
+	p['V0'] = 100
 
 	# Model parameters (FULL):
-	p = {}
-	L = 105
-	p['lambda'] = 2.5e8
-	p['k'] = 1e3
-	p['beta'] = 5e-13
-	p['mu'] = 2e-5*L
-	p['d'] = 1e-3
-	p['a'] = 1.
-	p['u'] = 3.
-	p['V0'] = 1
+	#p = {}
+	#L = 105
+	#p['lambda'] = 2.5e8
+	#p['k'] = 1e3
+	#p['beta'] = 5e-13
+	#p['mu'] = 2e-5*L
+	#p['d'] = 1e-3
+	#p['a'] = 1.
+	#p['u'] = 3.
+	#p['V0'] = 1
 
 	# Simulation parameters:
-	T = 10.
+	T = 30.
 	Nt = 1001
-	Nsamples = 101
+	Nsamples = 1001
 
 	# Derived simulation parameters:
 	dt = T/(Nt-1)
