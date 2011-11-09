be74132701e54d712d74a2d33a29ba820b027cad
Tim Vaughan <tgvaughan@gmail.com>
2011-11-09 18:57:13 +1100

    Another unproductive day. (!!)

--- tauleap_PST/tauleap_PST_SemiImplicit.cc
+++ tauleap_PST/tauleap_PST_SemiImplicit.cc
@@ -29,6 +29,17 @@ double SemiImplicit::unif2poisson(double lambda, double u)
 	return boost::math::quantile(boost::math::poisson(lambda), u);
 }
 
+/**
+ * Increment state vector using Peter's semi-implicit algorithm.
+ *
+ * sv:         StateVec to increment
+ * reactions:  array of reactions
+ * Nreactions: number of reactions in array
+ * tau:        size of time step
+ * buf:        RNG buffer
+ *
+ * returns: true if negative populations were generated, false otherwise.
+ */
 bool SemiImplicit::step(StateVec & sv, Reaction *reactions, int Nreactions, double tau, unsigned short  *buf)
 {
 	bool negativePop = false;
